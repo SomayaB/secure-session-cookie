b0eda59be137b153d5dd3fb57b71fbbbc5d2ba97
SomayaB <somaya.bounouar@gmail.com>
2017-08-23 11:19:17 -0700

    Complete app1

--- app1/app.js
+++ app1/app.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 let port = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.set('views', './views') //what does this line do?
+app.set('views', './views')
 app.set('view engine', 'pug')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
 app.get('/', (request, response) => {
-  response.send('Hello World')
+  const name = request.cookies.name
+  response.render('index', {name})
 })
 
+app.post('/', (request, response) => {
+  const name = request.body.name
+  response.cookie('name', name)
+  response.render('index', {name})
+})
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
